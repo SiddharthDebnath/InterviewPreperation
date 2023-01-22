d6c14f273c2819952643b2c757254c3b587011f4
Sid <sid6teen@gmail.com>
2023-01-22 12:16:46 -0600

    Fibonacci complete...

--- Recursion/Fibonacci_Numbers .py
+++ Recursion/Fibonacci_Numbers .py
@@ -1,18 +1,26 @@
 # Head fibonacci
 def head_fibonacci(n):
     if n == 0:
-        return 0
-    if n == 1:
-        return 1
+        result = 0
+        return result
+    elif n == 1:
+        result = 1
+        return result
+    else:
+        result = head_fibonacci(n - 1) + head_fibonacci(n - 2)
+        return result
 
-    fib1 = head_fibonacci(n-1)
-    fib2 = head_fibonacci(n-2)
 
-    result = fib1 + fib2
+print(head_fibonacci(7))
 
-    return result
 
 # Tail Fibonacci
-def tail_fibonacci(s):
-    if s == 0:
-        return 0
+def tail_fibonacci(n, a=0, b=1):
+    if n == 0:
+        return a
+    if n == 1:
+        return b
+
+    return tail_fibonacci(n - 1, b, a + b)
+
+print(tail_fibonacci(7))
