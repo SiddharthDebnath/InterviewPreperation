0427cb8869c67fd39b49d6721fbaa703937f98fa
Sid <sid6teen@gmail.com>
2023-01-28 00:30:09 -0600

    Linear_Search

--- Search_Algorithms/Linear_Search.py
+++ Search_Algorithms/Linear_Search.py
@@ -10,3 +10,18 @@ num = [1, 67, 38, 83, 43]
 value = 38
 
 print(linear_search(num, value))
+
+
+def linear_search_recursive(container, item, index=0):
+    if index >= len(container):
+        return -1
+
+        # base case when we find the item
+    if container[index] == item:
+        return index
+
+    return linear_search_recursive(container, item, index + 1)
+
+
+nums = [1, 4, 6, -4, 0, 100]
+print(linear_search_recursive(nums, 0))
